81866d1d956b1b27976b4d71e4aa0d17651e36aa
Muhammad-Abdullah786 <abdulx125@gmail.com>
2025-02-02 02:45:12 +0500

    done with image design

--- src/component/full-image-reusabel.tsx
+++ src/component/full-image-reusabel.tsx
@@ -5,8 +5,23 @@ export default async function FullPageImageView(props: { photoId: number }) {
   const image = await getImage(props.photoId);
 
   return (
-    <div>
-      <Image src={image.url} width={700} height={600} alt={image.name} />={" "}
+    <div className="flex h-screen items-center justify-center space-x-4 p-4">
+      {/* Left Side: Image */}
+      <div className="flex w-1/2 max-w-xl flex-shrink-0">
+        <Image
+          src={image.url}
+          width={700}
+          height={600}
+          className="w-full rounded-lg object-contain"
+          alt={image.name}
+        />
+      </div>
+
+      {/* Right Side: Text Information */}
+      <div className="flex w-1/2 flex-col justify-center space-y-4">
+        <p className="text-xl font-bold">{image.name}</p>
+        <p className="text-gray-500">Additional text here...</p>
+      </div>
     </div>
   );
 }
