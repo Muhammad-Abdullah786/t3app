00d086211ef8fd84faca865ebc694b2e0b7b24da
Muhammad-Abdullah786 <abdulx125@gmail.com>
2025-02-02 00:44:16 +0500

    fixed aerror

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,21 +1,47 @@
+// import Image from "next/image";
+// import { getImage } from "~/server/queries";
+// import { Modal } from "./modal";
+
+// export default async function PhotoModal({
+//   params: { id: photoId },
+// }: {
+//   params: { id: string };
+// }) {
+//   const idNumber = Number(photoId);
+//   if (Number.isNaN(idNumber))
+//     throw new Error(`invalid id its not a number!!!  🤬 `);
+//   const image = await getImage(idNumber);
+
+//   return (
+//     <div>
+//       <Modal>
+//         {/* <FullPageImageView photoId={photoId} /> */}
+//         <Image src={image.url} width={700} height={600} alt={image.name} />
+//       </Modal>
+//     </div>
+//   );
+// }
+
 import Image from "next/image";
 import { getImage } from "~/server/queries";
 import { Modal } from "./modal";
 
 export default async function PhotoModal({
-  params: { id: photoId },
+  params,
 }: {
   params: { id: string };
 }) {
+  const photoId = params.id; // Access params.id properly
+
   const idNumber = Number(photoId);
   if (Number.isNaN(idNumber))
-    throw new Error(`invalid id its not a number!!!  🤬 `);
+    throw new Error(`Invalid ID! It's not a number! 🤬`);
+
   const image = await getImage(idNumber);
 
   return (
-    <div className="flex h-full min-h-0 w-full min-w-0 overflow-y-hidden">
+    <div>
       <Modal>
-        {/* <FullPageImageView photoId={photoId} /> */}
         <Image src={image.url} width={700} height={600} alt={image.name} />
       </Modal>
     </div>
